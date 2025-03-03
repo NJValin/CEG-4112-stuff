57400abdc069bc205a7989d9aa00c806ffb433a4
Neil V <neilvalin1@gmail.com>
2025-03-02 19:26:26 -0500

    Worked on assignment 4

--- Assignment4/clustering_assignment.py
+++ Assignment4/clustering_assignment.py
@@ -1,28 +1,37 @@
 import pandas as pd
 from datasets import load_dataset
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
-from sklearn.cluster import SpectralClustering
+from sklearn.cluster import SpectralClustering, KMeans
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
-from umap import UMAP
+from sklearn.decomposition import PCA
 from icecream import ic
 
 def preprocess(X):
-    tfidf = TfidfVectorizer(stop_words='english', max_features=5_000)
+    tfidf = TfidfVectorizer(stop_words='english', max_features=1_000)
     X_transformed = tfidf.fit_transform(X)
-    ic(X_transformed)
     return X_transformed
 
 def apply_umap(X, target_components):
-    reducer = UMAP(n_neighbors=15, min_dist=0.1, n_components=target_components, random_state=42)
+    #reducer = UMAP(n_neighbors=15, min_dist=0.1, n_components=target_components, random_state=42)
+    reducer = PCA(n_components=target_components)
     embedding = reducer.fit_transform(X)
     return embedding
 
 def visualize_data(X,y):
-    twoD_df = apply_umap(X, 3)
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(twoD_df.iloc[:,0], twoD_df.iloc[:,1], twoD_df.iloc[:,2], c=y, cmap='viridis')
+    twoD_df = apply_umap(X, 2)
+    plt.scatter(
+        twoD_df[:,0],
+        twoD_df[:,1],
+        c=y,
+        cmap='viridis',
+        alpha=0.6
+    )
+    plt.xlabel("UMAP Component 1")
+    plt.ylabel("UMAP Component 2")
+    plt.colorbar()
     plt.show()
 
 def preprocess(X, y):
@@ -32,22 +41,23 @@ def preprocess(X, y):
     return X, y
 
 
-ds = load_dataset("dair-ai/emotion", "split")
-df = pd.DataFrame(ds['train'])
-ic(df.head())
-
-X = df['text'].values
-y = df['label'].values
+ds = load_dataset("dair-ai/emotion", split="train")
+X = ds["text"]
+y = ds["label"]
+print(y)
 X = preprocess(X)
-ic(X)
-X_1 = apply_umap(X, 500)
 
-#visualize_data(X, y)
+visualize_data(X, y)
 accuracies = []
 for i in range(1, 10):
     kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, random_state=0)
-    kmeans.fit(X_1)
-    y_kmeans = kmeans.predict(X_1)
+    y_kmeans = kmeans.fit_predict(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y_kmeans, test_size=0.2, random_state=42)
+    
+    classifier = RandomForestClassifier(max_depth=10)
+    classifier.fit(X_train, y_train)
+    y_label_pred = classifier.predict(X_test)
     accuracy = accuracy_score(y, y_kmeans)
     accuracies.append(accuracy)
     print(accuracy)
