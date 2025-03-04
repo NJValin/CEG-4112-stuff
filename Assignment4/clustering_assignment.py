3fb2c605f44c623431db6f753b1ed323a6b9e4ea
NJValin <neilvalin1@gmail.com>
2025-03-03 22:23:20 -0500

    worked more on assignment 4

--- Assignment4/clustering_assignment.py
+++ Assignment4/clustering_assignment.py
@@ -1,18 +1,23 @@
 import pandas as pd
 from datasets import load_dataset
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.cluster import SpectralClustering, KMeans
+from sklearn.metrics import accuracy_score, classification_report, explained_variance_score
+from sklearn.cluster import KMeans, DBSCAN
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MaxAbsScaler, StandardScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
 from icecream import ic
 
 def preprocess(X):
     tfidf = TfidfVectorizer(stop_words='english', max_features=1_000)
+    minmax = MaxAbsScaler()
+    reducer = PCA(n_components=2)
     X_transformed = tfidf.fit_transform(X)
-    return X_transformed
+    X_tr = minmax.fit_transform(X_transformed)
+    X_reduced = reducer.fit_transform(X_tr)
+    return X_reduced
 
 def apply_umap(X, target_components):
     #reducer = UMAP(n_neighbors=15, min_dist=0.1, n_components=target_components, random_state=42)
@@ -34,36 +39,39 @@ def visualize_data(X,y):
     plt.colorbar()
     plt.show()
 
-def preprocess(X, y):
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
-    apply_umap(X,y)
-    return X, y
 
 
 ds = load_dataset("dair-ai/emotion", split="train")
 X = ds["text"]
 y = ds["label"]
-print(y)
-X = preprocess(X)
+X= preprocess(X)
 
 visualize_data(X, y)
-accuracies = []
-for i in range(1, 10):
-    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, random_state=0)
-    y_kmeans = kmeans.fit_predict(X)
+#kmeans = KMeans(n_clusters=6, init='k-means++', max_iter=300, random_state=0)
+#y_kmeans = kmeans.fit_predict(X)
+dbs = DBSCAN(eps=0.001, min_samples=50, n_jobs=-1)
+y_db = dbs.fit_predict(X)
+visualize_data(X,y_db)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y_kmeans, test_size=0.2, random_state=42)
+accuracies = []
+k_values = range(1,10)
+for i in k_values:
+    #kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, random_state=0)
+    #y_kmeans = kmeans.fit_predict(X)
+    dbs = DBSCAN(eps=0.5, min_samples=i, n_jobs=-1)
+    y_db = dbs.fit_predict(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y_db, test_size=0.2, random_state=42)
     
     classifier = RandomForestClassifier(max_depth=10)
     classifier.fit(X_train, y_train)
     y_label_pred = classifier.predict(X_test)
-    accuracy = accuracy_score(y, y_kmeans)
+    accuracy = accuracy_score(y_test, y_label_pred)
     accuracies.append(accuracy)
     print(accuracy)
 
-
-    print(accuracy)
 plt.figure(figsize=(8,5)) 
-plt.plot(range(1,21), accuracy, marker='o', linestyle='--')
+plt.plot(k_values, accuracies, marker='o', linestyle='--')
+plt.ylabel("Classifier accuracies")
+plt.xlabel("number of clusters")
 plt.show()
